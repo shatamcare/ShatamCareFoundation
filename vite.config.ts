d6ae75dbdaa6f9d087ceb198417ae5b0457ee06b
ADARSH ALEX BALMUCHU <adarshalex.balmuchui23@iimranchi.ac.in>
2025-07-09 15:33:18 +0530

    website fixed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
   // Use base path for GitHub Pages in production
-  base: "/",
+  base: mode === 'production' ? "/ShatamCareFoundation/" : "/",
   server: {
     host: "::",
     port: 5173,
